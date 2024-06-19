1d5a7287c7a351f0062d1a2a5ec43e64b37c429e
XiaoNing <475529787@qq.com>
2024-06-19 21:34:43 +0800

    ✨ feat: setup 初体验

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class = "app">
-    <h1>你好啊</h1>
-    <Person />
-  </div>
+  <Person />
 </template>
 
 <script lang="ts">
@@ -18,10 +15,5 @@
 </script>
 
 <style>
-  .app {
-    background-color: #ddd;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 10px;
-  }
+
 </style>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -10,19 +10,26 @@
 <script lang="ts">
   export default {
     name: 'Person',
-    data() {
-      return {
-        name: '张三',
-        age: 18
+    setup() {
+      // 数据
+      // 此时的name和age不是响应式的
+      let name = '张三'
+      let age = 18
+
+      // 方法
+      function changeName() {
+        console.log('change name')
+        // 这样修改 name，页面没有变化
+        name = '李四'
+        // name 改了，但 name 不是响应式的
+        console.log(name)
       }
-    },
-    methods: {
-      changeName() {
-        this.name = '李四'
-      },
-      changeAge() {
-        this.age++
+
+      function changeAge() {
+        age++
       }
+
+      return { name, age, changeName, changeAge }
     }
   }
 </script>
