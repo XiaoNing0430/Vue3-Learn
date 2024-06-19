7ac8cf9e99b44ed0ddf9e6211f9ef9d704d2cdc2
XiaoNing <475529787@qq.com>
2024-06-19 20:40:23 +0800

    ✨ feat: vue3 兼容 vue2 语法

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div class = "app">
     <h1>你好啊</h1>
+    <Person />
   </div>
 </template>
 
 <script lang="ts">
+  import Person from './components/Person.vue'
+
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      // 注册组件
+      Person
+    }
   }
 </script>
 
---
+++ src/components/Person.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class = "person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click = "changeName">修改姓名</button>
+    <button @click = "changeAge">增加年龄</button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Person',
+    data() {
+      return {
+        name: '张三',
+        age: 18
+      }
+    },
+    methods: {
+      changeName() {
+        this.name = '李四'
+      },
+      changeAge() {
+        this.age++
+      }
+    }
+  }
+</script>
+
+<style>
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 10px;
+  }
+  button {
+    margin: 0 5px;
+  }
+</style>
