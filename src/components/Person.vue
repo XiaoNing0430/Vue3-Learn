554845a48e00b128edbdb413274358a3ac010544
XiaoNing <475529787@qq.com>
2024-06-22 21:16:58 +0800

    ✨ feat: toRefs和toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,31 +1,37 @@
 <template>
   <div class = "person">
-    <h2>品牌：{{ car.brand }}</h2>
-    <h2>价格：{{ car.price }}</h2>
-    <button @click = "changeBrand">修改品牌</button>
-    <button @click = "changePrice">增加价格</button>
+    <div>姓名：{{ name }}</div>
+    <div>姓名：{{ nl }}</div>
+    <div>姓名：{{ person.age }}</div>
+    <div>年龄：{{ age }}</div>
+
+    <button @click="changeName">修改姓名</button>
+    <button @click="changeAge">年龄+1</button>
   </div>
 </template>
 
 <!-- name属性可以自定义组件的名称，方便调试 -->
 <script setup lang="ts" name="Person">
-  import { ref } from 'vue'
+  import { reactive, toRefs, toRef } from 'vue'
 
-  // 数据
-  let car = ref ({
-    brand: '奔驰',
-    price: 1000000
+  let person = reactive({
+    name: '张三',
+    age: 18,
+    sex: '男'
   })
 
-  function changeBrand() {
-    car.value.brand = '宝马'
+  let { name, age } = toRefs(person)
+
+  let nl = toRef(person, 'name')
+  console.log(nl) 
+
+  function changeName() {
+    name.value += '-'
   }
 
-  function changePrice() {
-    car.value.price += 10000
+  function changeAge() {
+    age.value++
   }
-
-
 
 </script>
 
