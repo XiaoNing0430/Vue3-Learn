c8079c5e4db80e3319a38a6f77a087413975b832
XiaoNing <475529787@qq.com>
2024-06-22 17:00:05 +0800

    ✨ feat: reactive创建响应式对象

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,30 +1,24 @@
 <template>
   <div class = "person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>
-    <button @click = "changeName">修改姓名</button>
-    <button @click = "changeAge">增加年龄</button>
+    <h2>一辆 {{ car.name }} 车，价值 {{ car.price }} 元</h2>
+    <button @click="changeCarPrice">加价</button>
   </div>
 </template>
 
 <!-- name属性可以自定义组件的名称，方便调试 -->
 <script setup lang="ts" name="Person">
-  import { ref } from 'vue'
+  import { reactive } from 'vue'
 
   // 数据
-  // 此时的name和age是一个RefImpl的实例对象，简称ref对象，它们的value属性是响应式的。
-  let name = ref('张三')
-  let age = ref(18)
+  let car = reactive ({
+    name: 'BMW',
+    price: 1000000,
+    color: 'black'
+  })
 
   // 方法
-  function changeName() {
-    console.log('change name')
-    // // JS中操作ref对象时候需要.value
-    name.value = '李四'
-  }
-
-  function changeAge() {
-    age.value++
+  function changeCarPrice() {
+    car.price += 100000
   }
 </script>
 
