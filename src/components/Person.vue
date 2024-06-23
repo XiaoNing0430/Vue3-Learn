5d444e6a9fde1ea6b4bffe1c1c2c042459b2d241
XiaoNing <475529787@qq.com>
2024-06-23 16:32:56 +0800

    ✨ feat: watchEffect监视数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,60 +1,43 @@
 <template>
   <div class = "person">
-    <h1>情况四：</h1>
-    <h2>姓名：{{ person.name }}</h2>
-    <h2>年龄：{{ person.age }}</h2>
-    <h2>汽车品牌：{{ person.car.brand }}</h2>
-    <h2>汽车价格：{{ person.car.price }}</h2>
-    <button @click="changeName">修改姓名</button>
-    <button @click="changeAge">修改年龄</button>
-    <button @click="changeCarBrand">修改汽车品牌</button>
-    <button @click="changeCarPrice">修改汽车价格</button>
-    <button @click="changeCar">修改汽车</button>
+    <h2>水温：{{ temperature }}</h2>
+    <h2>水位：{{ height }}cm</h2>
+    <button @click="addTemperature">水温+10</button>
+    <button @click="addHeight">水位+10</button>
   </div>
 </template>
 
 <!-- name属性可以自定义组件的名称，方便调试 -->
 <script setup lang="ts" name="Person">
-  import { ref, reactive, watch } from 'vue'
+  import { ref, watch, watchEffect } from 'vue'
 
   // 数据
-  let person = reactive({
-    name: '张三',
-    age: 18,
-    car: {
-      brand: '奔驰',
-      price: 100000
-    }
-  })
+  let temperature = ref(0)
+  let height = ref(0)
 
   // 方法
-  function changeName() {
-    person.name += '~'
+  function addTemperature() {
+    temperature.value += 10
   }
 
-  function changeAge() {
-    person.age += 1
-  }
-
-  function changeCarBrand() {
-    person.car.brand += '~'
-  }
-
-  function changeCarPrice() {
-    person.car.price += 10000
-  }
-
-  function changeCar() {
-    person.car = {
-      brand: '宝马',
-      price: 200000
-    }
+  function addHeight() {
+    height.value += 10
   }
 
   // 监视
-  watch([() => person.name, () => person.car.brand], (newValue, oldValue) => {
-    console.log('姓名和汽车品牌发生变化', newValue, oldValue)
-  }, { deep: true })
+  /* watch([temperature, height], (newValue) => {
+    console.log('temperature、height变化了', newValue)
+    let [newTemperature, newHeight] = newValue
+    if(newTemperature >= 60 || newHeight >= 80) {
+      alert('报警')
+    }
+  }) */
+
+  watchEffect(() => {
+    if(temperature.value >= 60 || height.value >= 80) {
+      alert('报警')
+    }
+  })
 
 </script>
 
