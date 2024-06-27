a81ba575960b6d0a3e8ae67fcd2ccb541ab76dea
XiaoNing <475529787@qq.com>
2024-06-27 20:45:08 +0800

    ✨ feat: 路由的props属性

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 // 创建一个路由器，并暴露出去
 // 1. 引入 createRouter
+import { title } from "process"
 import { createRouter, createWebHistory } from "vue-router"
 
 // 2.创建路由器
@@ -22,7 +23,21 @@ const router = createRouter({
 				{
 					name: 'news-detail',
 					path: 'detail',
-					component: () => import('@/views/NewsDetail.vue')
+					component: () => import('@/views/NewsDetail.vue'),
+					// 第一种写法：将路由收到的所有params参数作为props都传递给组件
+					// props: true
+
+					// 第二种写法：函数写法，自己决定将什么作为props传给路由组件
+					props(route){
+						return route.query
+					}
+
+					// 第三种写法：对象写法，自己决定将什么作为props传给路由组件
+					/* props: {
+						id: '111',
+						title: '222',
+						content: '333'
+					} */
 				}
 			]
 		},
