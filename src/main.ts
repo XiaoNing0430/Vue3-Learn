7fed3ba1bcced7a95e8d1ab2cb1d38b08d6d8189
XiaoNing <475529787@qq.com>
2024-06-29 15:58:33 +0800

    搭建pinia环境

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@
 import { createApp } from 'vue'
 // 引入 App 根组件
 import App from './App.vue'
+// 1、引入pinia
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
+
+// 2、创建pinia实例
+const pinia = createPinia()
+// 3、挂载pinia
+app.use(pinia)
 
 // 挂载整个应用到app根元素上
 app.mount('#app')
